cf9fea83368aa1f289c2917eff1ff36e11feea66
Rajaram Padmanathan <rpadmanathan@paypal.com>
2019-02-01 11:28:02 -0800

    Release prep

--- tests/TestHarness.php
+++ tests/TestHarness.php
@@ -17,8 +17,8 @@ class TestHarness
     }
     public static function environment()
     {
-        $clientId = getenv("CLIENT_ID") ?: "AVNCVvV9oQ7qee5O8OW4LSngEeU1dI7lJAGCk91E_bjrXF2LXB2TK2ICXQuGtpcYSqs4mz1BMNQWuso1";
-        $clientSecret = getenv("CLIENT_SECRET") ?: "EDQzd81k-1z2thZw6typSPOTEjxC_QbJh6IithFQuXdRFc7BjVht5rQapPiTaFt5RC-HCa1ir6mi-H5l";
+        $clientId = getenv("CLIENT_ID") ?: "<<PAYPAL-CLIENT-ID>>";
+        $clientSecret = getenv("CLIENT_SECRET") ?: "<<PAYPAL-CLIENT-SECRET>>";
         return new SandboxEnvironment($clientId, $clientSecret);
     }
 }
