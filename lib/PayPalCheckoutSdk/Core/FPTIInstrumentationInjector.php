c7bcda85e761ee6fc8ca78f489d4fc4b3b138b4c
Tiffany <tiffanyrzhou@gmail.com>
2019-11-07 15:15:56 -0800

    Version 1.0.1

--- lib/PayPalCheckoutSdk/Core/FPTIInstrumentationInjector.php
+++ lib/PayPalCheckoutSdk/Core/FPTIInstrumentationInjector.php
@@ -9,7 +9,7 @@ class FPTIInstrumentationInjector implements Injector
     public function inject($request)
     {
         $request->headers["sdk_name"] = "Checkout SDK";
-        $request->headers["sdk_version"] = "1.0.0";
+        $request->headers["sdk_version"] = "1.0.1";
         $request->headers["sdk_tech_stack"] = "PHP " . PHP_VERSION;
         $request->headers["api_integration_type"] = "PAYPALSDK";
     }
